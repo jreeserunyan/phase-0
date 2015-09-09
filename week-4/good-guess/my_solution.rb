571a8af298d96538559d9126d9e0010f6c6de87a
jreeserunyan <jreeserunyan@gmail.com>
2015-09-09 15:09:03 -0500

    Add pair releases

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,16 @@
+# Good Guess
+def good_guess?(int)
+    if int = 42
+      puts == true
+    elsif int != 42
+    puts false
+  end
+end
+      
+good_guess? (42)
+
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
