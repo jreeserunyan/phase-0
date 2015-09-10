8d5e150c6a9c14e70550d783bc98b93b5f723c21
jreeserunyan <jreeserunyan@gmail.com>
2015-09-10 10:00:28 -0500

    Add pairing assignments

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,10 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def leap_year? (year)
+        (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
+end
---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,13 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def welcome(address)
+	if address .include?("CA")
+		p "Welcome to California"
+	else "You should move to California"
+	
+	end
+end
