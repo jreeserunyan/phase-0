432ceb46af2a58b5c9d5f0ac8944a4f911974a24
jreeserunyan <jreeserunyan@gmail.com>
2015-09-24 11:09:52 -0500

    Revise reflection

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -1,8 +1,8 @@
 # Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: the utmost confidence and no worries].
+# I spent [#1] hours on this challenge.
 
 # Pseudocode
 
@@ -45,20 +45,20 @@
 # Refactored Solution
 
 class Die
+	attr_reader :labels
+	attr_reader :sides
+
   def initialize(labels)
   	@labels = labels
   	@sides = labels.length
-  	raise ArgumentError.new("Number of sides less than 1") if @sides < 1
+  	raise ArgumentError.new("Number of sides must be 1 or more") if @sides < 1
   end
 
- attr_reader :labels
- attr_reader :sides
-
   def roll
-  	# @labels[rand(1..@sides) -1]
   	@labels.sample()
   end
 end
+
 
 
 
@@ -76,15 +76,16 @@ Did you need to change much logic to get this to work?
 
 What does this exercise teach you about making code that is easily changeable or modifiable? 
 * It can save time since you don't need to start from scratch.
-	But for me, I got a little stuck on what had been done and struggled a bit when trying to look at it from another angle.
-
+	But for me, I got a little stuck on what had been done before.
+	And it took me a minute to clear my head and look at it from another angle.
 What new methods did you learn when working on this challenge, if any?
 * .sample = chooses a random element of elements() from an array.
 
 What concepts about classes were you able to solidify in this challenge?
 * I think I have a better understanding of what attr_reader does. 
-	It auto creates the instance variable and related methods that return the value of the instance variable, named with a symbol.
-	Which still means what I thought it did, that all that info is hidden inside the attr_rader and it's :(symbol)
+	It auto creates the instance variable and related methods that return the value of the instance variable.
+	It's named with a symbol. Which still means what I thought it did:
+	that all that info is hidden inside the attr_rader and it's :(symbol)
 	But the first time I just did it because the guy in the video did it.
 =end
 
