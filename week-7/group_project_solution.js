a6b5d68cec9836cbe45cdac4318f6cf31b4b5b74
jreeserunyan <jreeserunyan@gmail.com>
2015-10-01 19:25:52 -0500

    Add person 2 for group

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,18 @@
+// pseudocode
+// input: array of numbers
+// output: results of three SEPARATE functions (sum, mean and median)
+// ________________
+
+// create function for sum of all numbers in array
+//      add each number in array
+//      return total
+
+// create function for mean of all numbers in array
+//      add each number in array ( sum)
+//      divide “sum” by number of elements in array 
+//      return mean
+
+// create function for median of all numbers in array
+//      sort numbers from smallest to largest
+//      find the middle value
+//      return median
