2713870df6ef306750a6adf45872049728308a36
jreeserunyan <jreeserunyan@gmail.com>
2015-10-14 10:40:39 -0500

    Start pairing work

---
+++ week-9/dom-manipulation/home_page.js
@@ -0,0 +1,38 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: Irina and Lauren].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+
+
+// Release 1:
+document.getElementById("release-0").className = "done";
+
+// Release 2:
+document.getElementById("release-1").style.display = "none";
+
+// Release 3:
+document.getElementsByTagName("h1")[0].innerHTML = "I complete release 2.";
+
+// Release 4:
+document.getElementById("release-3").style.backgroundColor = 955251;
+
+
+// Release 5:
+document.getElementsByClassName("release-4")[0].style.fontSize = "2em";
+
+// Release 6:
+var tmpl = document.getElementById("hidden");
+document.body.appendChild(tmpl.content.cloneNode(true));
+
+
+// Release 7: Reflect
+// Create a reflection section in your "home-page.js" file. In it, answer the following questions:
+
+// What did you learn about the DOM?
+// What are some useful methods to use to manipulate the DOM?
---
+++ week-9/jquery/jquery-example.js
@@ -0,0 +1,59 @@
+// U3.W9:JQuery
+
+
+// I worked on this challenge [by myself, with: ].
+// This challenge took me [#] hours.
+
+$(document).ready(function(){
+
+//RELEASE 1:
+  //link the image
+
+//RELEASE 2:
+
+  //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
+
+$('body').css({'background-color': 'silver'})
+
+//RELEASE 3:
+  //Add code here to select elements of the DOM
+bodyElement = $('body')
+h1Element = $('h1')
+divElement = $('div')
+imgElement = $('img')
+
+//RELEASE 4:
+  // Add code here to modify the css and html of DOM elements
+
+$('h1:first').css({'color': 'teal'});
+$('h1:last').html('Bobolinks');
+
+//RELEASE 5: Event Listener
+  // Add the code for the event listener here
+$("img").on("mouseover", function(e){
+	e.preventDefault()
+	$(this).attr("src", "https://jreeserunyan.github.io/imgs/bobolink_Icon.png"
+)
+// $("img").on("mouseclick", "src", "https://jreeserunyan.github.io/blog/imgs/icon_sm2.png"
+)
+	// $("img").mouseleave() 
+
+ //        $("img").fadeTo("fast", 0.5);
+
+ //    }
+})
+
+
+// 	$("img").on("mouseleave", function(e){
+// 	e.preventDefault()
+// 	$(this).attr("src", "dbc_logo.png"
+// )
+
+//RELEASE 6: Experiment on your own
+
+
+
+
+
+
+})  // end of the document.ready function: do not remove or write DOM manipulation below this.
