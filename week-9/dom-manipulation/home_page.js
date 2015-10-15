6b80349b216fd5541afdf46f00fe92e54d5bd884
jreeserunyan <jreeserunyan@gmail.com>
2015-10-15 12:15:11 -0500

    Add pair 1

--- week-9/dom-manipulation/home_page.js
+++ week-9/dom-manipulation/home_page.js
@@ -1,38 +1,48 @@
 // DOM Manipulation Challenge
-
-
 // I worked on this challenge [by myself, with: Irina and Lauren].
-
 
 // Add your JavaScript calls to this page:
 
 // Release 0:
+// The releases are numbered out of order. But here is the assignment.
 
-
-
-// Release 1:
+// Release 0:
 document.getElementById("release-0").className = "done";
 
-// Release 2:
+// Release 1:
 document.getElementById("release-1").style.display = "none";
 
-// Release 3:
+// Release 2:
 document.getElementsByTagName("h1")[0].innerHTML = "I complete release 2.";
 
-// Release 4:
+// Release 3:
 document.getElementById("release-3").style.backgroundColor = 955251;
 
 
-// Release 5:
+// Release 4:
 document.getElementsByClassName("release-4")[0].style.fontSize = "2em";
 
-// Release 6:
+// Release 5:
 var tmpl = document.getElementById("hidden");
 document.body.appendChild(tmpl.content.cloneNode(true));
 
+// Release 6:
+// The releases are numbered differently in the assignment in this project file.
+
+
 
 // Release 7: Reflect
-// Create a reflection section in your "home-page.js" file. In it, answer the following questions:
-
-// What did you learn about the DOM?
+// What did you learn about the DOM? 
 // What are some useful methods to use to manipulate the DOM?
+
+// There are a few basic ways to access elements through the DOM.
+	// 	getElementById
+	// 	getElementsByTagName
+	// 	getElementsByName
+	// 	getElementsByClassName
+	// 	querySelector
+// We have the ability to change text (content and format), 
+// display options and even rearrange features. 
+// When accessing a single element that shares(could share?)
+// tags with other elements, an index is needed to get to that
+// specific place. Each call starts with "document".
